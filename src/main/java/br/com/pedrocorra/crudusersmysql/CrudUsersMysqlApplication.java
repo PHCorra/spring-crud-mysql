342c422a464a2017df6570d86d4d7ef1b504d22e
Pedro Henrique de Paula Corrá <pedro.corra@dataside.com.br>
2023-02-10 15:52:41 -0300

    Initialsetup for the project

---
+++ src/main/java/br/com/pedrocorra/crudusersmysql/CrudUsersMysqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrocorra.crudusersmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsersMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsersMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedrocorra/crudusersmysql/CrudUsersMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedrocorra.crudusersmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsersMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
